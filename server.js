41707e2db4cdeeddcec95032b884e9c9556a3d2b
Leliya <98910653+Leliya@users.noreply.github.com>
2023-02-17 09:36:55 +0400

    bugfix: server response

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(filepath));
 
 app.get('*', (_, res) => {
-  res.sendFile(filepath);
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
 app.listen(PORT);
